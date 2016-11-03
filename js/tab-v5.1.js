7d6d0f75321826740635032799f9f7ac0a2ed623
mico.wang <82043525@163.com>
2016-11-03 17:51:40 +0800

    modeController plugin study

--- js/tab-v5.1.js
+++ js/tab-v5.1.js
@@ -3,9 +3,10 @@
     $.fn.tab = function(options) {
         // 将defaults 和 options 参数合并到{}
         var opts = $.extend({},$.fn.tab.defaults,options);
-      
         return this.each(function() {
-            var obj = $(this);            
+            var obj = $(this);
+            var html = new EJS({url:'./temp5.1.ejs'}).render(opts.data)
+            $(obj)[0].innerHTML = html;   
             $(obj).find('.tab_header li').on(opts.trigger_event_type, function (){
                 $(obj).find('.tab_header li').removeClass('active');
                 $(this).addClass('active');
@@ -21,6 +22,7 @@
 
     //定义默认
     $.fn.tab.defaults = {
+        data:[],
         trigger_event_type:'click', //mouseover | click 默认是click
         change: function(index){
             console.log('current index = ' + index);
